843900292d359fdd31683590c880947c0aa564f5
azriel1rf <43790845+azriel1rf@users.noreply.github.com>
2021-09-30 19:41:09 +0900

    Format Python scripts (#39)
    * Black (formatter) setting
    
    * run `$ black .` at ./python/
    
    * format manually
    
    * modified .editorconfig for Python codes

--- python/phevaluator/table_tests/test_hashtable6.py
+++ python/phevaluator/table_tests/test_hashtable6.py
@@ -5,14 +5,14 @@ from evaluator.hashtable6 import NO_FLUSH_6
 
 
 class TestNoFlush6Table(BaseTestNoFlushTable):
-  TOCOMPARE = NO_FLUSH_6
-  TABLE = [0] * len(TOCOMPARE)
-  VISIT = [0] * len(TOCOMPARE)
-  NUM_CARDS = 6
+    TOCOMPARE = NO_FLUSH_6
+    TABLE = [0] * len(TOCOMPARE)
+    VISIT = [0] * len(TOCOMPARE)
+    NUM_CARDS = 6
 
-  def test_noflush6_table(self):
-    self.assertListEqual(self.TABLE, self.TOCOMPARE)
+    def test_noflush6_table(self):
+        self.assertListEqual(self.TABLE, self.TOCOMPARE)
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
--- python/phevaluator/table_tests/test_hashtable7.py
+++ python/phevaluator/table_tests/test_hashtable7.py
@@ -5,14 +5,14 @@ from evaluator.hashtable7 import NO_FLUSH_7
 
 
 class TestNoFlush7Table(BaseTestNoFlushTable):
-  TOCOMPARE = NO_FLUSH_7
-  TABLE = [0] * len(TOCOMPARE)
-  VISIT = [0] * len(TOCOMPARE)
-  NUM_CARDS = 7
+    TOCOMPARE = NO_FLUSH_7
+    TABLE = [0] * len(TOCOMPARE)
+    VISIT = [0] * len(TOCOMPARE)
+    NUM_CARDS = 7
 
-  def test_noflush7_table(self):
-    self.assertListEqual(self.TABLE, self.TOCOMPARE)
+    def test_noflush7_table(self):
+        self.assertListEqual(self.TABLE, self.TOCOMPARE)
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
