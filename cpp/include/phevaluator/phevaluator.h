bc204ff9aef836bdffacf612acd0a47ffa606334
Eli Stark <elistark.tech@gmail.com>
2022-12-04 11:22:47 -0800

    feat: remove omaha and classes to reduce size

--- cpp/include/phevaluator/phevaluator.h
+++ cpp/include/phevaluator/phevaluator.h
@@ -14,13 +14,7 @@
  *  limitations under the License.
  */
 
-
-#ifndef PHEVALUATOR_H
 #define PHEVALUATOR_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 /*
  * Return an integer indicating the rank of the hand. The strongest hand
@@ -59,51 +53,7 @@ extern "C" {
  * | A | 48| 49| 50| 51|
  *
  */
+
 int evaluate_5cards(int a, int b, int c, int d, int e);
 int evaluate_6cards(int a, int b, int c, int d, int e, int f);
 int evaluate_7cards(int a, int b, int c, int d, int e, int f, int g);
-
-/*
- * The first five parameters are the community cards on the board
- * The last four parameters are the hole cards of the player
- */
-int evaluate_omaha_cards(int c1, int c2, int c3, int c4, int c5,
-                         int h1, int h2, int h3, int h4);
-
-#ifdef __cplusplus
-} // closing brace for extern "C"
-#endif
-
-#ifdef __cplusplus
-
-#include <phevaluator/card.h>
-#include <phevaluator/hand.h>
-#include <phevaluator/rank.h>
-
-namespace phevaluator {
-
-Rank EvaluateCards(const Card& a, const Card& b, const Card& c, const Card& d,
-                   const Card& e);
-
-Rank EvaluateCards(const Card& a, const Card& b, const Card& c, const Card& d,
-                   const Card& e, const Card& f);
-
-Rank EvaluateCards(const Card& a, const Card& b, const Card& c, const Card& d,
-                   const Card& e, const Card& f, const Card& g);
-
-/*
- * The first five parameters are the community cards on the board
- * The last four parameters are the hole cards of the player
- */
-Rank EvaluateOmahaCards(const Card& c1, const Card& c2, const Card& c3,
-                        const Card& c4, const Card& c5,
-                        const Card& h1, const Card& h2, const Card& h3, const Card& h4);
-
-
-Rank EvaluateHand(const Hand& hand);
-
-} // namespace phevaluator
-
-#endif // __cplusplus
-
-#endif // PHEVALUATOR_H
--- python/phevaluator/utils.py
+++
@@ -1,7 +0,0 @@
-"""Utilities."""
-import random
-
-
-def sample_cards(size: int) -> list[int]:
-    """Sample random cards with size."""
-    return random.sample(range(52), k=size)
